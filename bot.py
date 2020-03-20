9ce00f3ec0571d7151338a53e7bb50b91e84985b
Artun Duman <aduman@demonware.net>
2020-03-19 17:59:15 -0700

    Adding compare and refactoring

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
-import re
 import json
+import humanize
+from collections import OrderedDict
 
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
@@ -9,21 +10,48 @@ from countries import Countries
 app = Flask(__name__)
 base_url = 'https://corona.lmao.ninja'
 
+important_keys = OrderedDict(
+    [('cases', 'cases'),
+     ('active', 'active'),
+     ('deaths', 'deaths'),
+     ('recovered', 'recovered'),
+     ('todayDeaths', 'deaths today'),
+     ('todayCases', 'new cases today'),
+     ('casesPerOneMillion', 'per million cases'),
+     ])
+
 
 def get_help_string():
     string = 'Try typing ' \
-          '"Info about Canada and Turkey\n" ' \
-          'or "World info"\n'
+          '"Info about Canada and Turkey"' \
+          ', "World info" or "Compare Iran and China"\n'
     return string
 
 
 def get_stats(resp):
     resp_json = json.loads(resp.content)
-    ret = "Cases: {}\nDeaths: {}\nRecovered: {}\n".format(
-        resp_json['cases'],
-        resp_json['deaths'],
-        resp_json['recovered']
-    )
+    template = '{}: {}\n'
+    ret = ''
+    for key in important_keys.keys():
+        numeric_value = resp_json.get(key, '')
+        if numeric_value != '':
+            ret += template.format(
+                important_keys[key].capitalize(),
+                humanize.intcomma(int(numeric_value))
+            )
+    return ret, resp_json
+
+
+def _compare(country1, country2, raw1, raw2):
+    template = '{} has more {}\n'
+    ret = ''
+    for key in important_keys.keys():
+        if raw1[key] > raw2[key]:
+            ret += template.format(country1.capitalize(), important_keys[key])
+        elif raw2[key] > raw1[key]:
+            ret += template.format(country2.capitalize(), important_keys[key])
+        else:
+            ret += 'Countries have same {}'.format(important_keys[key])
     return ret
 
 
@@ -33,19 +61,22 @@ def bot():
     msg_resp = MessagingResponse()
     msg = msg_resp.message()
     print(incoming_msg)
-    countries = [c for c in re.split(r'[;,."\'\s]\s*', incoming_msg) if c in Countries.values()]
+    countries = [c for c in Countries.values() if c in incoming_msg]
+
     print('Countries: {}'.format(countries))
     if 'help' in incoming_msg:
         msg.body(get_help_string())
+    elif 'north korea' in incoming_msg:
+        msg.body('North korea is a black box baby')
     elif len(countries) == 0:
         query_url = base_url + '/all'
         resp = requests.get(query_url)
         print(resp.content)
-        if resp.status_code == 200:
-            stats = get_stats(resp)
-            print(stats)
+        if resp.status_code == 200 and b'not found' not in resp.content:
+            print(resp.content)
+            stats_str, _ = get_stats(resp)
             msg.body(
-                "World stats of Coronavirus as of today:\n" + get_stats(resp)
+                "World stats of COVID-19 as of now:\n" + stats_str
             )
         else:
             msg.body("No data found for the given input. Might be a server error\n"
@@ -53,11 +84,28 @@ def bot():
     else:
         ret_string = ''
         count = 0
+        raw_entries = []
         for country in countries:
             resp = requests.get(base_url + '/countries/{}'.format(country))
-            if resp.status_code == 200:
+            print(resp.content)
+            if resp.status_code == 200 and b'not found' not in resp.content:
                 count += 1
-                ret_string += "{}'s stats of Coronavirus as of today:\n".format(country) + get_stats(resp)
+                stats_str, raw = get_stats(resp)
+                raw_entries.append(raw)
+                ret_string += "COVID-19 statistics of {} as of now:\n".format(country.capitalize()) + stats_str
+        if 'compare' in incoming_msg:
+            if len(countries) != 2:
+                ret_string = 'To compare countries, you should give two country names.\n' \
+                             'Given countries are {}'.format(', '.join(countries))
+            else:
+                comparison_str = _compare(
+                    countries[0],
+                    countries[1],
+                    raw_entries[0],
+                    raw_entries[1]
+                )
+                ret_string += comparison_str
+
         if count != 0:
             msg.body(ret_string)
         else:
